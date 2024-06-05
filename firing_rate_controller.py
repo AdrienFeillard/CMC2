de4e20d0c1edb1b7c9a4755cadf4d64dc17e66cc
AdrienFeillard <92714776+AdrienFeillard@users.noreply.github.com>
2024-06-05 16:09:20 +0200

    Add files via upload

--- firing_rate_controller.py
+++ firing_rate_controller.py
@@ -22,11 +22,11 @@ class FiringRateController:
             self.n_iterations)
         self.pars = pars
 
-        self.n_eq = self.n_neurons * 6 + self.n_muscle_cells * 2  # number of equations: number of CPG eq+muscle cells eq+sensors eq
-        self.muscle_l = 6 * self.n_neurons + 2 * np.arange(0, self.n_muscle_cells)  # muscle cells left indexes
-        self.muscle_r = self.muscle_l + 1  # muscle cells right indexes
-        self.all_muscles = 6 * self.n_neurons + np.arange(0, 2 * self.n_muscle_cells)  # all muscle cells indexes
-
+        self.n_eq = self.n_neurons*4 + self.n_muscle_cells*2 + self.n_neurons * 2  # number of equations: number of CPG eq+muscle cells eq+sensors eq
+        self.muscle_l = 4*self.n_neurons + 2 * np.arange(0, self.n_muscle_cells)  # muscle cells left indexes
+        self.muscle_r = self.muscle_l+1  # muscle cells right indexes
+        self.all_muscles = 4*self.n_neurons + np.arange(0, 2*self.n_muscle_cells)
+        # all muscle cells indexes
         # Vector of indexes for the CPG activity variables
         self.left_v = range(self.n_neurons)
         self.right_v = range(self.n_neurons, 2 * self.n_neurons)
@@ -99,6 +99,7 @@ class FiringRateController:
         """Euler step"""
         self.state[iteration + 1, :] = self.state[iteration, :] + \
                                        timestep * self.f(time, self.state[iteration], pos=pos)
+
         return np.concatenate([
             self.zeros8,  # the first 4 passive joints
             self.motor_output(iteration),  # the active joints
@@ -107,15 +108,10 @@ class FiringRateController:
 
     def step_euler_maruyama(self, iteration, time, timestep, pos=None):
         """Euler Maruyama step"""
-        self.state[iteration + 1, :] = self.state[iteration, :] + \
-                                       timestep * self.f(time, self.state[iteration], pos=pos)
-        self.noise_vec = self.get_ou_noise_process_dw(
-            timestep, self.noise_vec, self.pars.noise_sigma)
+        self.state[iteration + 1, :] = self.state[iteration, :] + timestep * self.f(time, self.state[iteration], pos=pos)
+        self.noise_vec = self.get_ou_noise_process_dw(timestep, self.noise_vec, self.pars.noise_sigma)
         self.state[iteration + 1, list(self.left_v) + list(self.right_v)] += self.noise_vec
-        self.state[iteration + 1,
-        self.all_muscles] = np.maximum(self.state[iteration + 1,
-        self.all_muscles],
-                                       0)  # prevent from negative muscle activations
+        self.state[iteration + 1, self.all_muscles] = np.maximum(self.state[iteration + 1, self.all_muscles], 0)  # prevent from negative muscle activations
         return np.concatenate([
             self.zeros8,  # the first 4 passive joints
             self.motor_output(iteration),  # the active joints
@@ -131,6 +127,9 @@ class FiringRateController:
         """
         left_muscles = self.state[iteration, self.left_m] * self.pars.act_strength
         right_muscles = self.state[iteration, self.right_m] * self.pars.act_strength
+
+        self.state[iteration, self.muscle_l] = left_muscles
+        self.state[iteration, self.muscle_r] = right_muscles
         return np.ravel(np.column_stack((left_muscles, right_muscles)))
 
     def ode_rhs(self, _time, state, pos=None):
@@ -150,8 +149,17 @@ class FiringRateController:
         # Connectivity matrices
         W_in = np.zeros((self.n_neurons, self.n_neurons))
         W_mc = np.zeros((self.n_muscle_cells, self.n_neurons))
-
+        """
         # Fill in W_in (CPG to CPG connections)
+        for i in range(self.n_neurons):
+            W_in[i, i:min(i + self.pars.n_asc + 1, self.n_neurons)] = 1 / np.arange(1, min(self.pars.n_asc + 1, self.n_neurons - i) + 1)
+            W_in[i, max(i - self.pars.n_desc, 0):i] = 1 / np.arange(1, i - max(i - self.pars.n_desc, 0) + 1)[::-1]
+
+        # Fill in W_mc (CPG to muscle cell connections)
+        for i in range(self.n_muscle_cells):
+            W_mc[i, 5 * i:5 * (i + 1)] = 1
+
+        """
         for i in range(50):
             for j in range(50):
                 if i <= j and j - i <= self.pars.n_asc:
@@ -172,19 +180,16 @@ class FiringRateController:
         # CPG dynamics
         input_left = self.pars.I - self.pars.b * state[self.left_a]
         input_right = self.pars.I - self.pars.b * state[self.right_a]
-        self.dstate[self.left_v] = (-state[self.left_v] + np.sqrt(np.maximum(
-            input_left - self.pars.w_inh * np.multiply(W_in.dot(np.ones(state[self.right_v].shape)),
-                                                       state[self.right_v]), 0))) / self.pars.tau
-        self.dstate[self.right_v] = (-state[self.right_v] + np.sqrt(np.maximum(
-            input_right - self.pars.w_inh * np.multiply(W_in.dot(np.ones(state[self.left_v].shape)),
-                                                        state[self.left_v]), 0))) / self.pars.tau
 
+        self.dstate[self.left_v] = (-state[self.left_v] + np.sqrt(np.maximum(
+            input_left - self.pars.w_inh * (W_in @ state[self.right_v]), 0))) / self.pars.tau
+        self.dstate[self.right_v] = (-state[self.right_v] + np.sqrt(np.maximum(
+            input_right - self.pars.w_inh * (W_in @ state[self.left_v]), 0))) / self.pars.tau
+        
         # Muscle dynamics
-        self.dstate[self.left_m] = (
-                    self.pars.w_V2a2muscle * W_mc.dot(state[self.left_v]) * (1 - state[self.left_m]) / self.pars.taum_a
-                    - state[self.left_m] / self.pars.taum_d)
-        self.dstate[self.right_m] = (self.pars.w_V2a2muscle * W_mc.dot(state[self.right_v]) * (
-                    1 - state[self.right_m]) / self.pars.taum_a
+        self.dstate[self.left_m] = (self.pars.w_V2a2muscle * (W_mc @ state[self.left_v]) * (1 - state[self.left_m]) / self.pars.taum_a
+                                    - state[self.left_m] / self.pars.taum_d)
+        self.dstate[self.right_m] = (self.pars.w_V2a2muscle * (W_mc @ state[self.right_v]) * (1 - state[self.right_m]) / self.pars.taum_a
                                      - state[self.right_m] / self.pars.taum_d)
 
         return self.dstate
